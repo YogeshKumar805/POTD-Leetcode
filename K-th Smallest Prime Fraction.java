f7d65bd65cddc91141faa5fe6cf975cc78a99727
Yogesh Kumar <143583251+YogeshKumar805@users.noreply.github.com>
2024-05-10 13:15:10 +0530

    Create K-th Smallest Prime Fraction.java
    You are given a sorted integer array arr containing 1 and prime numbers, where all the integers of arr are unique. You are also given an integer k.
    
    For every i and j where 0 <= i < j < arr.length, we consider the fraction arr[i] / arr[j].
    
    Return the kth smallest fraction considered. Return your answer as an array of integers of size 2, where answer[0] == arr[i] and answer[1] == arr[j].
    
     
    
    Example 1:
    
    Input: arr = [1,2,3,5], k = 3
    Output: [2,5]
    Explanation: The fractions to be considered in sorted order are:
    1/5, 1/3, 2/5, 1/2, 3/5, and 2/3.
    The third fraction is 2/5.
    Example 2:
    
    Input: arr = [1,7], k = 1
    Output: [1,7]
     
    
    Constraints:
    
    2 <= arr.length <= 1000
    1 <= arr[i] <= 3 * 104
    arr[0] == 1
    arr[i] is a prime number for i > 0.
    All the numbers of arr are unique and sorted in strictly increasing order.
    1 <= k <= arr.length * (arr.length - 1) / 2
     
    
    Follow up: Can you solve the problem with better than O(n2) complexity?

---
+++ K-th Smallest Prime Fraction.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] kthSmallestPrimeFraction(int[] arr, int k) {
+        
+        int n = arr.length;
+        
+        PriorityQueue<int[]> maxHeap = new PriorityQueue<>((x, y) -> Double.compare((double)y[0] / y[1], (double)x[0] / x[1]));
+        
+        for (int i = 0 ; i < n ; i++) {
+            for  (int j = i+1 ; j < n ; j++) {
+                maxHeap.add(new int[] {arr[i], arr[j]});
+                if (maxHeap.size() > k) maxHeap.poll();
+            }
+        }
+        
+        return maxHeap.peek();
+    }
+}

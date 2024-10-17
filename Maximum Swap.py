68b0d9a6f9b9ae729429b8a1949b0ee6357c31e2
Yogesh Kumar <143583251+YogeshKumar805@users.noreply.github.com>
2024-10-17 09:38:51 +0530

    Create Maximum Swap.py
    You are given an integer num. You can swap two digits at most once to get the maximum valued number.
    
    Return the maximum valued number you can get.
    
     
    
    Example 1:
    
    Input: num = 2736
    Output: 7236
    Explanation: Swap the number 2 and the number 7.
    Example 2:
    
    Input: num = 9973
    Output: 9973
    Explanation: No swap.
     
    
    Constraints:
    
    0 <= num <= 108

---
+++ Maximum Swap.py
@@ -0,0 +1,12 @@
+class Solution:
+    def maximumSwap(self, num: int) -> int:
+        nums=list(map(int,str(num)))
+        print(nums)
+        for i in range(len(nums)-1):
+            if nums[i]>=max(nums[i+1:]):
+                continue
+            else:
+                j=len(nums)-1-nums[::-1].index(max(nums[i+1:]))
+                nums[i],nums[j]=nums[j],nums[i]
+                break
+        return int(''.join(map(str,nums)))

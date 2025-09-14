880d2fccfd1e349fe32b8a5655a4dadf70ea7d0c
Yogesh Kumar <143583251+YogeshKumar805@users.noreply.github.com>
2025-09-14 13:26:10 +0530

    Create Vowel Spellchecker.py
    Given a wordlist, we want to implement a spellchecker that converts a query word into a correct word.
    
    For a given query word, the spell checker handles two categories of spelling mistakes:
    
    Capitalization: If the query matches a word in the wordlist (case-insensitive), then the query word is returned with the same case as the case in the wordlist.
    Example: wordlist = ["yellow"], query = "YellOw": correct = "yellow"
    Example: wordlist = ["Yellow"], query = "yellow": correct = "Yellow"
    Example: wordlist = ["yellow"], query = "yellow": correct = "yellow"
    Vowel Errors: If after replacing the vowels ('a', 'e', 'i', 'o', 'u') of the query word with any vowel individually, it matches a word in the wordlist (case-insensitive), then the query word is returned with the same case as the match in the wordlist.
    Example: wordlist = ["YellOw"], query = "yollow": correct = "YellOw"
    Example: wordlist = ["YellOw"], query = "yeellow": correct = "" (no match)
    Example: wordlist = ["YellOw"], query = "yllw": correct = "" (no match)
    In addition, the spell checker operates under the following precedence rules:
    
    When the query exactly matches a word in the wordlist (case-sensitive), you should return the same word back.
    When the query matches a word up to capitlization, you should return the first such match in the wordlist.
    When the query matches a word up to vowel errors, you should return the first such match in the wordlist.
    If the query has no matches in the wordlist, you should return the empty string.
    Given some queries, return a list of words answer, where answer[i] is the correct word for query = queries[i].
    
     
    
    Example 1:
    
    Input: wordlist = ["KiTe","kite","hare","Hare"], queries = ["kite","Kite","KiTe","Hare","HARE","Hear","hear","keti","keet","keto"]
    Output: ["kite","KiTe","KiTe","Hare","hare","","","KiTe","","KiTe"]
    Example 2:
    
    Input: wordlist = ["yellow"], queries = ["YellOw"]
    Output: ["yellow"]
     
    
    Constraints:
    
    1 <= wordlist.length, queries.length <= 5000
    1 <= wordlist[i].length, queries[i].length <= 7
    wordlist[i] and queries[i] consist only of only English letters.
     
    
    Seen this question in a real interview before?
    1/5
    Yes
    No
    Accepted
    70,277/125.3K
    Acceptance Rate
    56.1%
    Topics

---
+++ Vowel Spellchecker.py
@@ -0,0 +1,16 @@
+class Solution:
+    def spellchecker(self, wordlist: List[str], queries: List[str]) -> List[str]:
+        capital={i.lower():i for i in wordlist[::-1]}
+        vovel={''.join([j if j not in "aeiou" else '.' for j in i.lower()]):i for i in wordlist[::-1]}
+        wordlist=set(wordlist)
+        res=[]
+        for i in queries:
+            if i in wordlist:
+                res.append(i)
+            elif i.lower() in capital:
+                res.append(capital[i.lower()])
+            elif ''.join([j if j not in "aeiou" else '.' for j in i.lower()]) in vovel:
+                res.append(vovel[''.join([j if j not in "aeiou" else '.' for j in i.lower()])])
+            else:
+                res.append("")
+        return res

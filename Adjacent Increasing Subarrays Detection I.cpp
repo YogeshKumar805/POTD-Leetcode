a8eab1944b7135988b39a0396e036ebd9fdde7bf
Yogesh Kumar <143583251+YogeshKumar805@users.noreply.github.com>
2025-10-14 10:38:02 +0530

    Create Adjacent Increasing Subarrays Detection I.cpp
    Given an array nums of n integers and an integer k, determine whether there exist two adjacent subarrays of length k such that both subarrays are strictly increasing. Specifically, check if there are two subarrays starting at indices a and b (a < b), where:
    
    Both subarrays nums[a..a + k - 1] and nums[b..b + k - 1] are strictly increasing.
    The subarrays must be adjacent, meaning b = a + k.
    Return true if it is possible to find two such subarrays, and false otherwise.
    
     
    
    Example 1:
    
    Input: nums = [2,5,7,8,9,2,3,4,3,1], k = 3
    
    Output: true
    
    Explanation:
    
    The subarray starting at index 2 is [7, 8, 9], which is strictly increasing.
    The subarray starting at index 5 is [2, 3, 4], which is also strictly increasing.
    These two subarrays are adjacent, so the result is true.
    Example 2:
    
    Input: nums = [1,2,3,4,4,4,4,5,6,7], k = 5
    
    Output: false
    
     
    
    Constraints:
    
    2 <= nums.length <= 100
    1 < 2 * k <= nums.length
    -1000 <= nums[i] <= 1000

---
+++ Adjacent Increasing Subarrays Detection I.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    static bool hasIncreasingSubarrays(vector<int>& nums, int k) {
+        const int n=nums.size();
+        int len=1, prev=0;
+        for (int i=1; i<n && max(len/2, min(len, prev))<k; i++){
+            if (nums[i]>nums[i-1]) len++; 
+            else{
+                prev=len;
+                len=1;
+            }
+        }
+        return max(len/2, min(len, prev))>=k;
+        
+    }
+};

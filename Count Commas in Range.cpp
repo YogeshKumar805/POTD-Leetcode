0a6f26a87e44552a8e2013765c34f0e53fb94c30
Yogesh Kumar <143583251+YogeshKumar805@users.noreply.github.com>
2026-09-08 13:03:13 +0530

    Create Count Commas in Range.cpp
    You are given an integer n.
    
    Return the total number of commas used when writing all integers from [1, n] (inclusive) in standard number formatting.
    
    In standard formatting:
    
    A comma is inserted after every three digits from the right.
    Numbers with fewer than 4 digits contain no commas.
     
    
    Example 1:
    
    Input: n = 1002
    
    Output: 3
    
    Explanation:
    
    The numbers "1,000", "1,001", and "1,002" each contain one comma, giving a total of 3.
    
    Example 2:
    
    Input: n = 998
    
    Output: 0
    
    Explanation:
    
    All numbers from 1 to 998 have fewer than four digits. Therefore, no commas are used.
    
     
    
    Constraints:
    
    1 <= n <= 105
     
    Discover more
    Get Language Courses
    Try Life Coaching
    Seen this question in a real interview before?
    1/6
    Yes
    No
    Accepted
    115,621/154.6K
    Acceptance Rate
    74.8%
    Topics
    icon
    Companies
    Hint 1
    Discussion (119)
    
    
    Choose a type
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    Copyright © 2026 LeetCode. All rights reserved.

---
+++ Count Commas in Range.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int countCommas(int n) {
+        int count = 0;
+
+        for (int i = 1; i <= n; i++) {
+            if (i >= 1000) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+};
